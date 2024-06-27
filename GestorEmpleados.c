eb972226539190d3fba29ab94661de495e6e3a57
Joabriel <Quijanojoaquingabriel@gmail.com>
2024-06-27 19:38:55 -0300

    Aumenta el ID al agregar empleado.

--- GestorEmpleados.c
+++ GestorEmpleados.c
@@ -1,181 +1,127 @@
-#include <stdio.h> 
-#define CANTIDAD 2
+#include <stdio.h>
+#include <stdlib.h>  
+#define CANT_ADMIT 10
+#define ARCHIVONOMBRE "empleados.dat"
 
-struct empleados{
-    char apeynom[20];
-    int empNum;
+typedef struct{
+	int empNum;
+	char apeynom[20];
     char direccion[20];
     double sueldo;
-};
+}Empleados;
 
 enum opcs{
-	e='e',
-	l='l',
 	a='a',
+	l='l',
 	b='b',
-};/*
-enum opcs2{
-	todos=0,
-	num=1
-};*/
+	//(Actualizar).
+};
 
-/*
-//Prototipos de funciï¿½n.
-void CasoL();
-void CasoA();
-void CasoB();
-*/
-
-void Seleccion(char selec){
-	FILE *ArchivoenUso;
-	struct empleados emp[CANTIDAD];
-    switch(selec){
-	case e: 
-	ArchivoenUso = fopen("empleados.txt","wb");
-	if(ArchivoenUso == NULL){
-		perror("error al abrir archivo");
-		//return 1;
-	}
-
-	emp->empNum = 1;
-	printf("\nIngrese informaciÃ³n sobre el empleado: %i",emp->empNum);
-	fputs("               ----Datos de empleados----",ArchivoenUso);
-	fprintf(ArchivoenUso, "\nEmpleado NÂ°:		%i\n",emp->empNum);
-	printf("\n\nNombre y Apellido: ");
-	scanf("%s",&emp->apeynom);
-	fprintf(ArchivoenUso, "Nombre y Apellido:		%s\n", emp->apeynom);
-	getchar();
-
-	printf("\nDirecciÃ³n: ");
-	scanf("%s",&emp->direccion);
-	fprintf(ArchivoenUso, "DirecciÃ³n:		%s\n", emp->direccion);
-	getchar();
-
-	printf("\nSueldo: ");
-	scanf("%d",&emp->sueldo);
-	fprintf(ArchivoenUso, "Sueldo:			%d\n", emp->sueldo);
-	getchar();
-
-
-	break;
-	case l:	
-	/*printf("\n[todos]: 'todos'\n");
-	printf("[num]: ingresar numero de empleado:\n");
-	int selec2;
-	scanf("%i",&selec2);
-	CasoL(selec2);*/
-	getchar();
-	ArchivoenUso = fopen("empleados.txt", "rb");
-	if(ArchivoenUso == NULL){
-		printf("Error: Archivo Vacio.");
-	}
-	char caracter;
-	while(feof(ArchivoenUso) == 0){
-		caracter = fgetc(ArchivoenUso);
-		printf("%c",caracter);
-	};
-	break;
-/*
-	case a:
-		getchar();
-		ArchivoenUso = fopen("empleados.dat","rb")
-		if(ArchivoenUso == NULL){
-			perror("Error en la apertura del archivo");
-			return 1;
-		}
-		//.
-		printf("\nIngrese nÃºmero de empleado para ACTUALIZAR informaciÃ³n:\n");
-		int x;
-		scanf("%i",&x);
-
-		bool numeroEncontrado = false;
-		int capNum;
-
-		while(feof(ArchivoenUso) == 0 || numeroEncontrado == true){
-			fscanf(ArchivoenUso, "%i", &capNum);
-			if(capNum == x){
-				numeroEncontrado = true;
-				printf("Que informaciÃ³n desea actualizar de: %i?\n",emp.empNum);
-				printf("0=[Nombre]  1=[DirecciÃ³n]  2=[Sueldo]");
-
-				switch(x){
-					case 0:
-					scanf("%s",&emp[x].empNum);
-
-					break;
-					case 1: 
-					scanf("%s",&emp[x].direccion);
-					break;
-					case 2: 
-					scanf("%f",&emp[x].sueldo);
-				}
-			}
-		}
-
-
-
-		//CasoA(x);
-        break;
-	case b:
-		getchar();
+int obtenerUltimoId();
+void agregarDato();
+void leerDato();
+	
+	int main(){
+		printf("\n    ----Gestor de Empleados----\n");
+		printf("Nombre del archivo: %s\n\n",ARCHIVONOMBRE);	
+		printf("Opciones:\n");
+		printf("[ll]:'leer' ");
+		printf("[aa]:'Agregar Empleado.' \n\n");
+		//printf("[bb]:'borrar'\n");
 		
-        printf("\ningrese numero de empleado:\n");
-		int y;
-		scanf("%i",&y);
-		//CasoB(y);
-
-		break;
-	default:
-		perror("\nError: Operaciï¿½n Invï¿½lida.\n");
-		break;*/
-    }
-}
-
-/*
-void CasoL(int selec2){
-	struct empleados usaremp[CANTIDAD];
-	switch(selec2){
-	case 0: //(todos). 
-	//Se debe usar archivos, tienen que tener varios empleados.
-		for(int i=0; i<CANTIDAD; i++){
-			printf("\n%i\n",usaremp[i].empNum);
-			printf("%s\n",usaremp[i].apeynom);
-			printf("%s\n",usaremp[i].direccion);
-			printf("%d\n",usaremp[i].sueldo);
+		char selec;
+		scanf("%c",&selec);
+	
+		switch(selec){
+		case a:
+			agregarDato(ARCHIVONOMBRE);
+			break;  
+		case l: 
+			leerDato();
+			break;
+		default: 
+			printf("operación inválida.");
+			break;
 		}
-	case 1: //(num).
-		//ingresar nï¿½mero del empleado
-		printf("Ingrese Nï¿½mero del empleado:");
-		int x;
-		scanf("%i",&x);
-		printf("%s",usaremp[x].apeynom);
-		printf("%i",usaremp[x].empNum);
-		printf("%s",usaremp[x].direccion);
-		printf("%f",usaremp[x].sueldo);
-	default:
-		printf("\nError: Operaciï¿½n Invï¿½lida.");
-	};
-}
-	
-void CasoA(int x){
-}
-	
-void CasoB(int y){
-}
-*/
-//3ra versiÃ³n.
-int main(){
-	//getchar();
-    printf("\n    ----Gestor de Empleados----\n\n");
-    printf("Opciones:\n");
-	printf("[ee]: 'escribir'");
-    printf("[ll]:'leer' ");
-    printf("[aa]:'actualizar' ");
-    printf("[bb]:'borrar'\n\n");
-	//Paso 1: menÃº, seleccion de opciÃ³n y llevar a Seleccion();.
-	char selec;
-    scanf("%c",&selec);
-    Seleccion(selec);
+
 	getchar(); //Evitar que el programa se cierre solo.
-	return 0;
+	return 0;	
 }
+		
+	int obtenerUltimoId(const char *archivonombre){
+		FILE *VerNum = fopen(archivonombre,"rb");
+		if(VerNum == NULL){
+			fprintf(stderr,"No se pudo abrir el archivo para ver el ultimo número.");
+			return 0;
+		}
+		
+		Empleados emp;
+		int ultimoID = 0;
+		
+		while(fread(&emp,sizeof(Empleados),1,VerNum)==1){
+				ultimoID = emp.empNum;
+			}
+			
+		fclose(VerNum);
+		return ultimoID;
+	}
+		
+	void agregarDato(const char *archivonombre){
+		FILE *ArchivoenUso = fopen(archivonombre,"ab+");
+		if(ArchivoenUso == NULL){
+			fprintf(stderr, "No se pudo abrir el archivo para agregar datos\n");
+			exit(0);
+		}
+		
+		Empleados addEmp;
+		
+		
+		int ultimoid = obtenerUltimoId(ARCHIVONOMBRE);
+		addEmp.empNum = ultimoid +1;
+		
+		if(addEmp.empNum > CANT_ADMIT){
+			printf("No se puede agregar mas datos.");
+			exit(0);
+		}
+		
+			printf("\nEmpleado N°: %i\n",addEmp.empNum);
+			printf("Nombre_Apellido: ");
+			scanf("%s",&addEmp.apeynom);
+			getchar();
+			
+			printf("Dirección: ");
+			scanf("%s",&addEmp.direccion);
+			getchar();
+			
+			printf("Sueldo: ");
+			scanf("%d",addEmp.sueldo);
+			getchar();	
+
+		printf("\n-Fin de petición de datos.");
+		
+		fwrite(&addEmp,sizeof(Empleados),1,ArchivoenUso);
+		
+		fclose(ArchivoenUso);
+	}
+		
+		void leerDato(){
+			FILE *ArchivoenUso = fopen(ARCHIVONOMBRE,"rb");
+			if(ArchivoenUso == NULL){
+				fprintf(stderr, "No se pudo abrir el archivo para leer datos\n");
+				exit(0);
+			}
+			
+			Empleados leerEmp;
+			while(fread(&leerEmp,sizeof(Empleados),1,ArchivoenUso)==1){
+				printf("___________________");
+				printf("\nEmpleado N°: [ %i ]\n",leerEmp.empNum);
+				printf("Nombre/Apellido: [ %s ]\n",leerEmp.apeynom);
+				printf("Dirección: [ %s ]\n",leerEmp.direccion);
+				printf("Sueldo: [ %f ]\n",leerEmp.sueldo);
+			}
+			printf("\n\n-Fin de lectura de datos.");
+			fclose(ArchivoenUso);
+		}
+	
+	
+	
